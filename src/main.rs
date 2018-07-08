3c5b5ac5b43cbfe6275df102dcddcbdace9a5970
Nathan Stocks <cleancut@github.com>
2018-07-07 21:57:21 -0600

    Done - 03 Scope

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use std::env;
+
+fn main() {
+    let mut args: Vec<String> = env::args().skip(1).collect();
+    if args.len() != 2 {
+        println!("Usage: duel NAME HOST");
+        return;
+    }
+    host = args.pop().unwrap();
+    name = args.pop().unwrap();
+    println!("{} {}", name, host);
+}
