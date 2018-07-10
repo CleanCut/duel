247ea3ce7e0645309536eb05fbc2967c960b990f
Nathan Stocks <cleancut@github.com>
2018-07-09 22:02:45 -0600

    Need to exit the process on parsing error.

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn parse_args() {
     let mut args: Vec<String> = env::args().skip(1).collect();
     if args.len() != 2 {
         println!("Usage: duel NAME HOST");
-        return;
+        std::process::exit(2);
     }
     let host = args.pop().unwrap();
     let name = args.pop().unwrap();
