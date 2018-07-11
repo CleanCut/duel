a7d8630e93c309d47ad3277e0cacd7b7322e32db
Nathan Stocks <cleancut@github.com>
2018-07-10 20:55:09 -0600

    06 - Scalar & Collection Types

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
 pub mod audio;
 
 pub use audio::audio_loop;
+
+pub fn parse_args() -> (String, String) {
+    let mut args: Vec<String> = std::env::args().skip(1).collect();
+    if args.len() != 2 {
+        println!("Usage: duel NAME HOST");
+        std::process::exit(2);
+    }
+    let host = args.pop().unwrap();
+    let name = args.pop().unwrap();
+    (name, host)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,13 @@
 extern crate duel;
 extern crate rusty_sword_arena;
 
-use duel::audio_loop;
+use duel::{audio_loop, parse_args};
 use rusty_sword_arena::VERSION;
-use std::env;
 
 fn main() {
-    parse_args();
+    let (name, host) = parse_args();
+    println!("Using rusty_sword_arena v{}, name: {}, host: {}",
+             VERSION, name, host);
     audio_loop();
 }
 
-fn parse_args() {
-    let mut args: Vec<String> = env::args().skip(1).collect();
-    if args.len() != 2 {
-        println!("Usage: duel NAME HOST");
-        std::process::exit(2);
-    }
-    let host = args.pop().unwrap();
-    let name = args.pop().unwrap();
-    println!("Using rusty_sword_arena v{}, name: {}, host: {}",
-             VERSION, name, host);
-}
