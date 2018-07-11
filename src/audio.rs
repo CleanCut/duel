c82cc1619051ff5e19ae271e350cf617d6b945eb
Nathan Stocks <cleancut@github.com>
2018-07-10 22:16:53 -0600

    07 - Control Flow & Threads

--- src/audio.rs
+++ src/audio.rs
@@ -1,3 +1,13 @@
-pub fn audio_loop() {
+use std::sync::mpsc::Receiver;
+
+pub fn audio_loop(audio_rx : Receiver<u8>) {
     println!("Audio system initialized.");
+    loop {
+        let choice = audio_rx.recv().unwrap();
+        // Is it time to quit?
+        if choice == 0 {
+            break;
+        }
+    }
+    println!("Audio system shutdown");
 }
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,23 @@ extern crate rusty_sword_arena;
 
 use duel::{audio_loop, parse_args};
 use rusty_sword_arena::VERSION;
+use std::sync::mpsc::channel;
+use std::thread::Builder;
 
 fn main() {
     let (name, host) = parse_args();
     println!("Using rusty_sword_arena v{}, name: {}, host: {}",
              VERSION, name, host);
-    audio_loop();
+
+    // Run the audio system in a separate thread
+    let (audio_tx, audio_rx) = channel::<u8>();
+    let audio_handle = Builder::new()
+        .name("Audio System".to_string())
+        .spawn(move|| audio_loop(audio_rx))
+        .unwrap();
+
+    // Shut down
+    let _ = audio_tx.send(0);
+    let _ = audio_handle.join();
 }
 
