413a04f9aa515c0dcc75ffed68cc36d03870f66b
Nathan Stocks <cleancut@github.com>
2018-07-14 18:43:22 -0600

    14 - Documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+//! This is the library for Duel.  Yay!
+
 extern crate impose;
 extern crate rusty_sword_arena;
 
@@ -6,6 +8,8 @@ pub mod player;
 
 pub use audio::audio_loop;
 
+/// `parse_args()` is a fancy function that parses arguments for you.  It expects the an argument
+/// for name and host.
 pub fn parse_args() -> (String, String) {
     let mut args: Vec<String> = std::env::args().skip(1).collect();
     if args.len() != 2 {
